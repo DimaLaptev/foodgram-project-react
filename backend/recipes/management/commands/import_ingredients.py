4ad3183c3f3a349bc8b870da70da6676baab2202
Shipilov Dmitriy <shipilenok1@gmail.com>
2023-08-26 03:28:51 +0300

    test CI 4

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,23 @@
+import json
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Import indegrient seeds'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str, help='path to file')
+
+    def handle(self, *args, **options):
+        json_file = options['json_file']
+        with open(json_file, 'r') as jsonfile:
+            ingredients = json.load(jsonfile)
+            for ingredient in ingredients:
+                Ingredient.objects.get_or_create(
+                    name=ingredient['name'],
+                    measurement_unit=ingredient['measurement_unit']
+                )
+
+        self.stdout.write(self.style.SUCCESS('Import was successful'))
---
+++ backend/recipes/management/commands/import_tags.py
@@ -0,0 +1,25 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Tag
+
+
+class Command(BaseCommand):
+    help = 'Import tag seeds'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str, help='path to file')
+
+    def handle(self, *args, **options):
+        json_file = options['json_file']
+        with open(json_file, 'r') as jsonfile:
+            tags = json.load(jsonfile)
+            for tag in tags:
+                Tag.objects.get_or_create(
+                    slug=tag['slug'],
+                    name=tag['name'],
+                    color=tag['color']
+                )
+
+        self.stdout.write(self.style.SUCCESS('Import was successful!'))
